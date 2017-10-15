6a37cc1ba4723668e71fd103ef375e500e8c68c6
Alex Rabarts <alexrabarts@gmail.com>
2017-10-16 00:18:35 +0100

    Tune lead generation

--- js/services/lead_generator.js
+++ js/services/lead_generator.js
@@ -1,7 +1,7 @@
 var LeadGenerator = {
   call: function () {
     var marketingLevel = AppStore.marketingLevel();
-    let damping = 10;
+    let damping = 20;
     let power = (1 + marketingLevel) / damping;
     var minLeads = 2 ** (marketingLevel * 0.5);
     var maxLeads = 2 ** (marketingLevel * 0.5 + power);
